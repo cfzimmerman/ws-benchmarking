59dd8f2d23b954e5e3feef10aa1c802b081ace98
cfzimmerman <coryzimmerman93@gmail.com>
2023-06-18 12:50:47 -0500

    finish rust echo server

--- rust_server/src/lib.rs
+++ rust_server/src/lib.rs
@@ -17,7 +17,7 @@ pub mod redis_io {
         pub async fn set_benchmark_payload(&mut self, pl: benchmark::Payload) {
             if let Err(res) = self
                 .client
-                .set::<&str, benchmark::Counters, i32>(&pl.id, pl.counters)
+                .set::<&str, benchmark::Counters, ()>(&pl.id, pl.counters)
                 .await
             {
                 eprintln!("add_benchmark_payload error: {:?}", res);
@@ -36,16 +36,10 @@ pub mod redis_io {
 }
 
 pub mod benchmark {
-    use std::sync::Arc;
-
-    use my_ws::ws::{
-        event::EventAction,
-        socket::{Socket, To},
-        ws_io::Message,
-    };
+    use my_ws::ws::{event::EventAction, socket::To, ws_io::Message};
     use redis_macros::{FromRedisValue, ToRedisArgs};
     use serde::{Deserialize, Serialize};
-    use serde_json::Value;
+    use std::sync::Arc;
 
     use crate::redis_io::RedisConn;
 
@@ -72,35 +66,32 @@ pub mod benchmark {
     /// sends it to Redis, pulls it from Redis, stringifies it, and returns it to the
     /// client.
     pub fn handle_message(redis: Arc<tokio::sync::Mutex<RedisConn>>) -> EventAction {
-        Box::new(
-            move |socket: Arc<tokio::sync::Mutex<Socket>>, message: serde_json::Value| {
-                let redis = redis.clone();
-                let payload = if let Ok(pl) = serde_json::from_value::<Payload>(message) {
-                    pl
-                } else {
-                    // eprintln!("unable to parse: {}", message);
-                    return;
-                };
-                tokio::spawn(async move {
-                    let mut rd = redis.lock().await;
-                    let socket = socket.lock().await;
+        Box::new(move |socket, message| {
+            let redis = redis.clone();
+            let payload = if let Ok(pl) = serde_json::from_value::<Payload>(message) {
+                pl
+            } else {
+                // eprintln!("unable to parse: {}", message);
+                return;
+            };
+            tokio::spawn(async move {
+                let mut rd = redis.lock().await;
+                let socket = socket.lock().await;
 
-                    let id = payload.id.clone();
-                    rd.set_benchmark_payload(payload).await;
-                    let res = rd.get_benchmark_payload(&id).await;
-                    match serde_json::to_string(&res.unwrap()) {
-                        Ok(res_str) => {
-                            if let Err(err) = socket.send(Message::Text(res_str), To::Origin).await
-                            {
-                                eprintln!("error sending message: {:?}", err);
-                            };
-                        }
-                        Err(err) => {
-                            eprintln!("parse error: {:?}", err);
-                        }
-                    };
-                });
-            },
-        )
+                let id = payload.id.clone();
+                rd.set_benchmark_payload(payload).await;
+                let res = rd.get_benchmark_payload(&id).await;
+                match serde_json::to_string(&res.unwrap()) {
+                    Ok(res_str) => {
+                        if let Err(err) = socket.send(Message::Text(res_str), To::Origin).await {
+                            eprintln!("error sending message: {:?}", err);
+                        };
+                    }
+                    Err(err) => {
+                        eprintln!("parse error: {:?}", err);
+                    }
+                };
+            });
+        })
     }
 }
